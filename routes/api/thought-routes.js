baa8d5845fa559702625b134f80a0caab7bc64b2
JCH95 <jhorger95@gmail.com>
2022-05-26 21:38:58 -0500

    added thought routes in routes dir

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+    getAllThought,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../..controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThought)
+    .get(createThought);
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router
+    .route('/:userId/reaction/:reactionId')
+    .post(addReaction)
+    .delete(deleteReaction);
+
+module.exports = router;
