0caa6981e03b00de07b9bf3ea7f41413367a381f
JCH95 <jhorger95@gmail.com>
2022-05-26 21:53:00 -0500

    added get user by id route in controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,28 @@
 const { User, Thought } = require('../models');
 
 const userController = {
-    // get all pizzas
     getAllUser(req, res) {
         User.find({})
             .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'thought',
+                path: 'friend',
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
