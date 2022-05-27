975835adc0aa8ae00a4f2b901489bbf585726b10
JCH95 <jhorger95@gmail.com>
2022-05-26 22:12:58 -0500

    added get all thought route to controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,20 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    getAllThought(req, res) {
+        Thought.find({})
+            .populate({
+                path: 'thought',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+};
+
+module.exports = thoughtController;
